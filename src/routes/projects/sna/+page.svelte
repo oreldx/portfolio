38e41d60e355b177b18daa64f352f59c212018a4
Orel <orel.dev@proton.me>
2024-08-15 16:06:35 +0200

    feat: :sparkles: Add SNA content details

--- src/routes/projects/sna/+page.svelte
+++ src/routes/projects/sna/+page.svelte
@@ -1,9 +1,18 @@
 <script>
     import Icon from "@iconify/svelte";
+    import { expoOut } from "svelte/easing";
     import { t } from "../../../lib/i18n.js";
+    import { blur } from "svelte/transition";
 </script>
 
-<div class="flex flex-col gap-4">
+<div
+    class="flex flex-col gap-4"
+    in:blur={{
+        amount: 5,
+        duration: 800,
+        easing: expoOut,
+    }}
+>
     <a class="flex gap-2 items-center hover:text-white" href="/projects">
         <Icon icon="bxs:left-arrow" class=" text-l" />
         <p>Back</p>
@@ -12,9 +21,33 @@
         <h2 class="text-white">
             {$t("projects.sna.title")}
         </h2>
-        <p>
-            {$t("projects.sna.desc")}
-        </p>
-        <p class="italic">Content in progress...</p>
+        <div class="flex flex-col gap-8">
+            <section>
+                <p class="text-justify">
+                    {@html $t("projects.sna.details.intro")}
+                </p>
+            </section>
+            <section>
+                <p class="text-justify">
+                    {@html $t("projects.sna.details.video")}
+                </p>
+                <div class="mt-4 flex flex-col items-center">
+                    <video
+                        class="px-8 lg:max-h-[50vh] md:max-h-[60vh]"
+                        src="/sna/video.mp4"
+                        muted
+                        autoplay
+                        loop
+                        controls
+                    />
+                    <p class="text-center text-sm">{$t("projects.sna.details.media.video")}</p>
+                </div>
+            </section>
+            <section>
+                <p class="text-justify">
+                    {@html $t("projects.sna.details.outro")}
+                </p>
+            </section>
+        </div>
     </div>
 </div>
